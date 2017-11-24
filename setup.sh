bad74400fe1f10ff71b1731e08b5013bbd7fb91b
Mohit Choudhary <mohit.choudhary@scripbox.com>
2017-11-24 21:11:39 +0530

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+install_for_ubuntu() {
+  sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+  sudo apt-get update
+  sudo apt-get install rcm
+}
+
+install_for_mac() {
+  brew tap thoughtbot/formulae
+  brew install rcm
+}
+
+install_thoughtbot_rcm() {
+  if type apt 2> /dev/null; then
+    echo "System: debian/ubuntu"
+  elif type brew 2> /dev/null; then
+    echo "System: mac"
+  else
+    echo "System not supported."
+  fi
+}
+
+if ! type rcup 2> /dev/null; then
+  install_thoughtbot_rcm
+fi
+
+RCRC=$HOME/dotfiles/rcrc rcup
